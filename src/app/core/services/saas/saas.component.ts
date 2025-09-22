a68c442a485d834ec3ffb869c158c1fb9a0d8159
Evandro <vando86206@gmail.com>
2025-09-22 17:11:08 -0300

    criaçãom da estrutura basica  de pasta com seus componetee e services e routes (home, login e register)

---
+++ src/app/core/services/saas/saas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saas',
+  imports: [],
+  templateUrl: './saas.component.html',
+  styleUrl: './saas.component.scss'
+})
+export class SaasComponent {
+
+}
